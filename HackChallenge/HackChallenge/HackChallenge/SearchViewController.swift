f4413c02f02007f19d378ac5fa3aaf9871affb97
xinhaohe245 <75575162+xinhaohe245@users.noreply.github.com>
2020-12-12 09:12:31 -0500

    Add files via upload

---
+++ HackChallenge/HackChallenge/HackChallenge/SearchViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SearchViewController.swift
+//  HackChallenge
+//
+//  Created by Xinhao He on 12/12/20.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
