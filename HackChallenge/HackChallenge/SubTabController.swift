600cf6ddc0f66cd0a12cc7341c5132a11baa9ba1
xinhaohe245 <75575162+xinhaohe245@users.noreply.github.com>
2020-12-11 20:46:25 -0500

    Update SubTabController.swift

--- HackChallenge/HackChallenge/SubTabController.swift
+++ HackChallenge/HackChallenge/SubTabController.swift
@@ -11,10 +11,9 @@ class SubTabController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let activityView = ActivityListController()
-        activityView.tabBarItem = UITabBarItem(title: "Activities", image: nil, tag: 1)
+//         activityView.tabBarItem = UITabBarItem(title: "Activities", image: nil, tag: 1)
         let foodView = FoodViewController()
-        foodView.tabBarItem = UITabBarItem(title: "Food", image: nil, tag: 2)
+//         foodView.tabBarItem = UITabBarItem(title: "Food", image: nil, tag: 2)
         self.viewControllers = [activityView, foodView]
-//        self.selectedViewController = activityView
     }
 }
